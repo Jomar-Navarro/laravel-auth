992e9da1fb099a8a3753c521cce5acc82db3afb9
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-21 15:40:39 +0200

    [fix] logout button

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,5 +1,5 @@
-    <header>
-    <nav class="navbar navbar-expand-lg bg-dark">
+<header>
+    <nav class="navbar navbar-expand-lg bg-dark p-3">
         <div class="container-fluid">
             <a class="navbar-brand text-white" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,18 +17,21 @@
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
 
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active text-white" aria-current="page" href="#">{{ Auth::user()->name }}</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Link</a>
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button class="btn btn-danger" type="submit">Logout</button>
+                        </form>
                     </li>
                 </ul>
-                </form>
 
             </div>
         </div>
     </nav>
-    </header>
+</header>
