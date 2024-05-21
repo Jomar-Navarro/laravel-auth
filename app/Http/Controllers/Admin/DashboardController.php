cbf76eb2007ade54e4b6d7c1b49d4d2a882d0e28
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-21 15:14:46 +0200

    [changed] RouteServiceProvider

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Http\Controllers\Guest;
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PageController extends Controller
+class DashboardController extends Controller
 {
     public function index(){
-        return view('guest.home');
+        dd('admin');
     }
 }
